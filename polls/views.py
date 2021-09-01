263b1cb3f7f973624d58d8c0bf6e7d71665908ac
Bernardo Morcon <bernardo.morcon@gmail.com>
2021-08-31 23:30:18 -0500

    simple views added

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, this is the index page")
+
+def detail(request, question_id):
+	return HttpResponse("question " % question_id)
+
+def results(request, question_id):
+	response = "result of question %s" 
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("voting on question %s." % question_id)
