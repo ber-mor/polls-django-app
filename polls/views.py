faca202504f97fa5d36fc59a16edf4a4c792fe26
Bernardo Morcon <bernardo.morcon@gmail.com>
2021-09-04 16:50:26 -0500

    updated views

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, this is the index page")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("question " % question_id)
 
 def results(request, question_id):
-	response = "result of question %s" 
+	response = "result of question %s." 
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
